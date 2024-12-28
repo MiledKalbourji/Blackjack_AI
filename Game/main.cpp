27f5565010cf156c75ba86f0b599d7c4efbba308
Miled Kalbourji <120202170+MiledKalbourji@users.noreply.github.com>
2024-12-28 10:45:37 -0500

    Add files via upload

---
+++ Game/main.cpp
@@ -0,0 +1,11 @@
+#include "blackjack.h"  // Include the header file that declares all classes
+
+int main() {
+    Game blackjackGame;  // Create a Game object
+
+    blackjackGame.play();  // Start the game
+
+    return 0;  // Indicate that the program ended successfully
+}
+	
+	
